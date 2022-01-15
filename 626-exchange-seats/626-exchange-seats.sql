9cade1f796d3a1f5520b6638f70f595d2d6d6073
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-01-15 21:35:10 +0530

    Time: 604 ms (5.34%), Space: 0B (100.00%) - LeetHub

--- 626-exchange-seats/626-exchange-seats.sql
+++ 626-exchange-seats/626-exchange-seats.sql
@@ -1,6 +1,1 @@
-# Write your MySQL query statement below
-# select ,s.student from (select id,student from seat where id%2=0) s,(select id,student from seat where id%2<>0) ss; 
-
-
 select if(s.id%2=0 or s.id<(select count(id) from seat),if(s.id%2=0,s.id-1,s.id+1),s.id) as id,s.student from seat as s order by id;
-# select s.student,ss.student from seat as s,seat as ss;
