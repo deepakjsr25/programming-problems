86d8d99b7adc9dbeb33401390eca7d8f9843d738
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-01-18 14:11:29 +0530

    Time: 6 ms (36.33%), Space: 7.8 MB (47.71%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> v;
+        map<int,int> m;
+        
+        // for(auto it:nums){
+        //     m[it]++;
+        // }
+        sort(nums.begin(),nums.end());
+        ans.push_back(v);
+        subset(nums,ans,v,0,nums.size());
+        
+        return ans;
+    }
+    
+    void subset(vector<int>& nums,vector<vector<int>>& ans,vector<int> v,int i,int n){
+        if(i==n){
+            return;
+        }
+       
+        int begin=i;
+        while(begin<n){
+            if(begin==i || (nums[begin]!=nums[begin-1])){
+                v.push_back(nums[begin]);
+                ans.push_back(v);
+                subset(nums,ans,v,begin+1,n);
+                v.pop_back();
+            }
+            begin++;
+        }
+        
+        return;
+    }
+};
