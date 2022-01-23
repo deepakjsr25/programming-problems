f2bf8b3bd5eb7fe8fc7da55248ab779bb912c0f5
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-01-23 17:09:18 +0530

    Time: 615 ms (18.59%), Space: 0B (100.00%) - LeetHub

--- 180-consecutive-numbers/180-consecutive-numbers.sql
+++ 180-consecutive-numbers/180-consecutive-numbers.sql
@@ -1,1 +1,9 @@
-select distinct(l1.num) as ConsecutiveNums from logs as l1,logs as l2,logs as l3 where (l1.num=l2.num and l2.num=l3.num) and (l2.id-l1.id=1 and l3.id-l2.id=1) order by ConsecutiveNums asc;
+select DISTINCT num as ConsecutiveNums FROM
+(select num,
+	case 
+		when @record = num then @count:=@count+1
+		when @record <> @record:=num then @count:=1 end as n
+    from 
+	    Logs ,(select @count:=0,@record:=(SELECT num from Logs limit 1)) r #this select statement is only for initialization it will only give you count:=0 and @record:=1;
+) a
+where a.n>=3;
