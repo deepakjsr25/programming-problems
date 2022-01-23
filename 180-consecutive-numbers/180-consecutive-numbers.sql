94ceeeb62a4315e393d3d7b3c220a4a40ab8c789
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-01-23 16:40:40 +0530

    Time: 448 ms (63.34%), Space: 0B (100.00%) - LeetHub

---
+++ 180-consecutive-numbers/180-consecutive-numbers.sql
@@ -0,0 +1,1 @@
+select distinct(l1.num) as ConsecutiveNums from logs as l1,logs as l2,logs as l3 where (l1.num=l2.num and l2.num=l3.num) and (l2.id-l1.id=1 and l3.id-l2.id=1) order by ConsecutiveNums asc;
