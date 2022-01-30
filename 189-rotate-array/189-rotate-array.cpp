f7ac5721ed5b589349b114c912995499cb2d642d
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-01-30 12:39:47 +0530

    Time: 63 ms (11.27%), Space: 24.8 MB (92.88%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -5,12 +5,9 @@ public:
         if(k>n){
             k=k%n;
         }
-        
+
         reverse(nums.begin(),nums.end());
         std::rotate(nums.begin(), nums.begin() + k , nums.end());
         reverse(nums.begin(),nums.end());
-        // while(k--){
-        //     std::rotate(nums.begin(), nums.begin() + n-1 , nums.end());
-        // }
     }
 };
