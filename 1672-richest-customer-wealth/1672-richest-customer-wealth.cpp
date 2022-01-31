9162da8eb68e8d85269d79e9f78f327774b67643
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-01-31 14:06:20 +0530

    Time: 4 ms (90.57%), Space: 7.9 MB (13.11%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -2,11 +2,8 @@ class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
         int maxi=INT_MIN;
-        int sum=0;
         for(auto it:accounts){
-            sum=0;
-            sum=accumulate(it.begin(), it.end(), 0);
-            maxi=max(maxi,sum);
+            maxi=max(maxi,accumulate(it.begin(), it.end(), 0));
         }
         
         return maxi;
