32e88d6e5e579f8853dff88e5b72d7e9585330fd
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-02-08 10:40:01 +0530

    Time: 3 ms (53.62%), Space: 5.9 MB (25.37%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int addDigits(int num) {
-        if(num==0){
-            return 0;
-        }
-        int sum = log10(num)+1;
-        
-        while(sum != 1){
-            sum=0;
-            while(num){
-                sum+=num%10;
+      int sum=0;
+        while(num>9)
+        {
+            while(num)
+            {
+                sum+=(num%10);
                 num/=10;
             }
             num=sum;
-            sum=log10(num)+1;
+            sum=0;
         }
         return num;
     }
