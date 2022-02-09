99daa4e1f97a1e11e633a2a60dd5a8c0ccc0c641
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-02-09 11:45:28 +0530

    Time: 116 ms (31.28%), Space: 27.2 MB (23.21%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -8,10 +8,10 @@ public:
         dp[len2][len1]=1;
         
         for(int j=len1-1;j>=0;j--){
-            dp[len2][j]=0;
+            dp[len2][j]=0;                     // last row
         }
         
-        for(int i=len2-1;i>=0;i--){
+        for(int i=len2-1;i>=0;i--){            // last column
             if(p[i] == '*'){
                 dp[i][len1] = dp[i+1][len1];                
             } 
@@ -32,34 +32,4 @@ public:
         
         return dp[0][0];
     }
-    
-//     bool matchexp(int i,int j,string s,string p)
-//     {
-//         int len1 = s.length();//7
-//         int len2 = p.length();//2
-//         if(i>=len1 and j>=len2){//1 1
-//             return 1;
-//         }
-        
-//         if(j>=len2)
-//             return 0;
-        
-//         // if(dp[start][from] != -1)
-//         //     return dp[start][from];
-        
-//         if(p[j]!='*' && s[i]!=p[j]){
-//             return 0;
-//         }
-        
-//         if(p[j]=='?' || s[i]==p[j]){
-//             return matchexp(i+1,j+1,s,p);
-//         }
-        
-//         int op=0;
-//         for(int k=i+1;k<len1;k++){//2   7
-//             op=matchexp(k,j,s,p) || op;// 2,1
-//         }
-        
-//         return op;
-//     }
 };
