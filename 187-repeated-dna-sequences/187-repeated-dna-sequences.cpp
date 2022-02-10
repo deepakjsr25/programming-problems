6cae4a10b7028c60d7f866fca84b83349722c2f0
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-02-10 17:00:53 +0530

    Time: 68 ms (71.17%), Space: 23.6 MB (10.22%) - LeetHub

--- 187-repeated-dna-sequences/187-repeated-dna-sequences.cpp
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.cpp
@@ -8,19 +8,12 @@ public:
             return ans;
         }
         
-        // if(n==10){
-        //     ans.push_back(s);
-        //     return ans;
-        // }
-        
         int i=9,j=0;
         m[s.substr(0,i-j+1)]=1;
-        // cout<<s.substr(0,i-j+1);
         i++;
         j++;
         while(i<n){
             string str = s.substr(j,i-j+1);
-            // cout<<str<<endl;
             if(!m[str]){
                 m[str]++;
             }
