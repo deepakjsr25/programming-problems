bb52ef18a2e0382c15a34e8fe70ca43b8981d5b4
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-02-24 21:12:04 +0530

    Time: 969 ms (5.02%), Space: 31.4 MB (44.26%) - LeetHub

--- 148-sort-list/148-sort-list.cpp
+++ 148-sort-list/148-sort-list.cpp
@@ -1,42 +1,49 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
-        if (!head || !head->next)
+        if(head==NULL){
             return head;
-        ListNode* mid = getMid(head);
-        ListNode* left = sortList(head);
-        ListNode* right = sortList(mid);
-        return merge(left, right);
-    }
-
-    ListNode* merge(ListNode* list1, ListNode* list2) {
-        ListNode dummyHead(0);
-        ListNode* ptr = &dummyHead;
-        while (list1 && list2) {
-            if (list1->val < list2->val) {
-                ptr->next = list1;
-                list1 = list1->next;
-            } else {
-                ptr->next = list2;
-                list2 = list2->next;
-            }
-            ptr = ptr->next;
         }
-        if(list1) ptr->next = list1;
-        else ptr->next = list2;
-
-        return dummyHead.next;
-    }
-
-    ListNode* getMid(ListNode* head) {
-        ListNode* midPrev = nullptr;
-        while (head && head->next) {
-            midPrev = (midPrev == nullptr) ? head : midPrev->next;
-            head = head->next->next;
+        
+        if(head->next==NULL){
+            return head;
         }
-        ListNode* mid = midPrev->next;
-        midPrev->next = nullptr;
-        return mid;
+        
+        ListNode* newnode = sortList(head->next);
+        
+        if(newnode->val >= head->val){
+            head->next=newnode;
+            return head;
+        }
+        
+        ListNode* prev=newnode;
+        ListNode* curr=newnode;
+        while(curr && curr->val<head->val){
+            prev=curr;
+            curr=curr->next;
+        }
+        
+        ListNode* temp=prev->next;
+        prev->next=head;
+        prev=prev->next;
+        
+        if(!temp){
+            prev->next=NULL;
+        }
+        else{
+            prev->next=temp;
+        }
+        
+        return newnode;
     }
 };
