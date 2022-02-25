5b194d652a751a9452be0dfca6f6193ac80851d8
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-02-25 15:39:03 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (35.61%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.cpp
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -1,50 +1,34 @@
 class Solution {
 public:
     int compareVersion(string version1, string version2) {
-        int n = version1.size();
-        int n2 = version2.size();
-        
-        
-        int i=0,j=0;
-        int s=0,s2=0;
-        
-        while(i<n){
-            if(version1[i]=='.'){
-                break;
-            }
-            s=s*10+(version1[i]-'0');
+    int i = 0; 
+    int j = 0;
+    int n1 = version1.size(); 
+    int n2 = version2.size();
+    
+    int num1 = 0;
+    int num2 = 0;
+    while(i<n1 || j<n2)
+    {
+        while(i<n1 && version1[i]!='.'){
+            num1 = num1*10+(version1[i]-'0');
             i++;
         }
         
-        while(j<n2){
-            if(version2[j]=='.'){
-                break;
-            }
-            s2=s2*10+(version2[j]-'0');
+        while(j<n2 && version2[j]!='.'){
+            num2 = num2*10+(version2[j]-'0');;
             j++;
         }
         
+        if(num1>num2) return 1;
+        else if(num1 < num2) return -1;
         
-        if(s>s2){
-            return 1;
-        }
-        
-        if(s==s2){
-            if(i==n && j==n2){
-                return 0;
-            }
-            
-            if(i==n){
-                return compareVersion("",version2.substr(j+1));
-            }
-            
-            if(j==n2){
-                return compareVersion(version1.substr(i+1),"");
-            }
-            
-            return compareVersion(version1.substr(i+1),version2.substr(j+1));
-        }
-        
-        return -1;
+        num1 = 0;
+        num2 = 0;
+        i++;
+        j++;
     }
+    
+    return 0;
+}
 };
