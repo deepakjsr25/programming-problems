97ea1cf151a5e1596d7b178012a843ae979fdea7
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-03-14 10:25:39 +0530

    Time: 20 ms (7.50%), Space: 9.5 MB (52.07%) - LeetHub

---
+++ 71-simplify-path/71-simplify-path.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    string simplifyPath(string path)
+    {
+        string res, tmp;
+        vector<string> stk;
+        stringstream ss(path);
+        while (getline(ss, tmp, '/'))
+        {
+            cout<<tmp<<endl;
+            if (tmp == "" or tmp == ".")
+                continue;
+            if (tmp == ".." and !stk.empty())
+                stk.pop_back();
+            else if (tmp != "..")
+                stk.push_back(tmp);
+        }
+        for (auto str : stk)
+            res += "/" + str;
+        return res.empty() ? "/" : res;
+    }
+};
