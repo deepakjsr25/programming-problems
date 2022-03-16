8b47958646be0ba72377ebe4992745dc1ba1518f
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-03-16 16:07:05 +0530

    Time: 253 ms (80.87%), Space: 0B (100.00%) - LeetHub

--- 626-exchange-seats/626-exchange-seats.sql
+++ 626-exchange-seats/626-exchange-seats.sql
@@ -1,7 +1,10 @@
-SELECT
-    s1.id, COALESCE(s2.student, s1.student) AS student
-FROM
-    seat s1
-        LEFT JOIN
-    seat s2 ON ((s1.id + 1) ^ 1) - 1 = s2.id
-ORDER BY s1.id;
+# SELECT
+#     s1.id, COALESCE(s2.student, s1.student) AS student
+# FROM
+#     seat s1
+#         LEFT JOIN
+#     seat s2 ON ((s1.id + 1) ^ 1) - 1 = s2.id
+# ORDER BY s1.id;
+
+
+select if(s1.id%2<>0,if((select s2.id from seat as s2 where s2.id=s1.id+1) is not null,s1.id+1,s1.id),s1.id-1) as id,student from seat as s1 order by id asc;
