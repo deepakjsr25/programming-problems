e82b8c0681f9d2ea5ad2d269c68ba75304c2fc89
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-03-17 12:32:22 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (45.05%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.cpp
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -3,7 +3,7 @@ public:
     int scoreOfParentheses(string s) {
         int n=s.size();
         int cnt=1,curr=0;
-        vector<int> v(n,1);
+        vector<int> dp(n,1);
         
         if(n==2){
             return 1;
@@ -11,18 +11,18 @@ public:
         
         for(int i=1;i<n;i++){            
             if(s[i-1]=='(' && s[i-1]==s[i]){
-                v[i]=v[i-1]*2;
+                dp[i]=dp[i-1]*2;
             }
             else if(s[i-1]==')' && s[i-1]!=s[i]){
                 if(!cnt){
-                    v[i]=1;
+                    dp[i]=1;
                 }
                 else{
-                    v[i]=pow(2,cnt);
+                    dp[i]=pow(2,cnt);
                 }
             }
             else{
-                v[i]=v[i-1];
+                dp[i]=dp[i-1];
             }
             
             if(s[i]=='('){
@@ -35,10 +35,8 @@ public:
         
         int sum=0;
         for(int i=0;i<n-1;i++){
-            // cout<<v[i]<<" ";
             if(s[i]=='(' && s[i+1]==')'){
-                sum+=v[i];
-                // cout<<v[i]<<" ";
+                sum+=dp[i];
             }
         }
         
