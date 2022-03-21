c9820d8f96df20eaa1ab4ba0b78941e715207e1a
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-03-21 19:39:44 +0530

    Time: 73 ms (75.00%), Space: 16.3 MB (75.00%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
@@ -2,20 +2,7 @@ class Solution {
 public:
     int countCollisions(string directions) {
         int n=directions.size();
-        // vector<int> v(n,0);
-        
-        // left=-1,right=1,stay=0
-        // -1,-1,1,1
         int coll=0;
-        // if(directions[0]=='S'){
-        //     v[0]=0;
-        // }
-        // else if(directions[0]=='R'){
-        //     v[0]=1;
-        // }
-        // else{
-        //     v[0]=-1;
-        // }
         int cnt=0;
         if(directions[0]=='R'){
             cnt++;
@@ -36,14 +23,6 @@ public:
                 coll+=1;
                 directions[i]='S';
             }
-            // else if(directions[i-1]=='R' && directions[i]=='S'){
-            //     int j=i-1;
-            //     while(j>=0 && directions[j]=='R'){
-            //         directions[j]=='S';
-            //         j--;
-            //         coll++;
-            //     }
-            // }
             
             coll+=cnt;
             cnt=0;
@@ -53,6 +32,3 @@ public:
     }
 };
 
-// "SSRSSRLLRSLLRSRSSRLRRRRLLRRLSSRR"
-// "SSSSSSSSSSSSSSSSSSSRRRSSSRSSSSRR"
-// C=16
