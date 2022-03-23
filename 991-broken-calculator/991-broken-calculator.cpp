008bae478fdffa7bffa704b37e5408cd875fb67c
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-03-23 16:04:20 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (73.18%) - LeetHub

--- 991-broken-calculator/991-broken-calculator.cpp
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int brokenCalc(int startValue, int target) {
-        
         // return startValue >= target ? startValue - target : 1 + target % 2 + brokenCalc(startValue, (target + 1) / 2);
+        
+        // My  solution;
         if(target<=startValue){
             return startValue-target; 
         }
@@ -11,21 +12,7 @@ public:
             return 1+brokenCalc(startValue, target+1);
         }
         
-        // if(target>=(startValue*2)){
-            return 1+brokenCalc(startValue, target/2);
-        // }
+        return 1+brokenCalc(startValue, target/2);
         
-        // int cal=startValue-target/2;
-        // int cal2=target+1-startValue;
-        // if(cal<=cal2){
-        //     return 1+brokenCalc(startValue, target/2);
-        // }
-        
-//         long long op1=1+brokenCalc(startValue, target/2);
-//         long long op2=1+brokenCalc(startValue, target+1);
-        
-//         return min(op1,op2);
-        
-        // return 0;
     }
 };
