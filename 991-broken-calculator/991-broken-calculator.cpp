dc2dac3d55782c4243199e05fe8164b5e8e8fd7d
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-03-23 16:03:21 +0530

    Time: 4 ms (27.86%), Space: 5.8 MB (95.31%) - LeetHub

--- 991-broken-calculator/991-broken-calculator.cpp
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
     int brokenCalc(int startValue, int target) {
         
-        return startValue >= target ? startValue - target : 1 + target % 2 + brokenCalc(startValue, (target + 1) / 2);
-//         if(target<=startValue){
-//             return startValue-target; 
-//         }
+        // return startValue >= target ? startValue - target : 1 + target % 2 + brokenCalc(startValue, (target + 1) / 2);
+        if(target<=startValue){
+            return startValue-target; 
+        }
         
-//         if((target&1)){
-//             return 1+brokenCalc(startValue, target+1);
-//         }
+        if((target&1)){
+            return 1+brokenCalc(startValue, target+1);
+        }
         
-//         if(target>=(startValue*2)){
-//             return 1+brokenCalc(startValue, target/2);
-//         }
+        // if(target>=(startValue*2)){
+            return 1+brokenCalc(startValue, target/2);
+        // }
         
-//         // int cal=startValue-target/2;
-//         // int cal2=target+1-startValue;
-//         // if(cal<=cal2){
-//         //     return 1+brokenCalc(startValue, target/2);
-//         // }
+        // int cal=startValue-target/2;
+        // int cal2=target+1-startValue;
+        // if(cal<=cal2){
+        //     return 1+brokenCalc(startValue, target/2);
+        // }
         
 //         long long op1=1+brokenCalc(startValue, target/2);
 //         long long op2=1+brokenCalc(startValue, target+1);
