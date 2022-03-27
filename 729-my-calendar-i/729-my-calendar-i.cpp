91351269de69efd59cfb7a5595d549413139b4f9
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-03-27 11:11:44 +0530

    Time: 290 ms (16.21%), Space: 48.5 MB (6.34%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,10 +1,7 @@
 class MyCalendar {
 public:
-    // int v[70000000];
     unordered_map<int,pair<int,int>> m;
-    // int i;
     MyCalendar() {
-        // memset(v,-1,70000);
     }
     
     bool book(int start, int end) {
@@ -17,16 +14,12 @@ public:
             return 1;
         }
         
-         // cout<<m[si].first<<" "<<l<<endl;
         if((m[si].first<l && l<m[si].second) || (m[si].first<r && r<m[si].second)){
-            // cout<<m[si].first<<" "<<l<<endl;
             return 0;
         }
-        // cout<<"2"<<endl;
         if(m[si].first>=r){
             return add(0,si*2+1,l,r);
         }
-        // cout<<"3"<<endl;
         if(m[si].second<=l){
             return add(0,si*2+2,l,r);
         }
