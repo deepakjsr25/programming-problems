b40e46a283a3700d87994bd7e6509b1f97d04f4f
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-04-02 11:20:20 +0530

    Time: 832 ms (10.33%), Space: 8.2 MB (10.44%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -1,57 +1,36 @@
 class Solution {
 public:
-    // Defined it as per the maximum size of array and split count
-    // But can be defined with the input size as well
-    int memo[1001][51];
-    
-    int getMinimumLargestSplitSum(vector<int>& prefixSum, int currIndex, int subarrayCount) {
-        int n = prefixSum.size() - 1;
-        
-        // We have already calculated the answer so no need to go into recursion
-        if (memo[currIndex][subarrayCount] != -1) {
-            return memo[currIndex][subarrayCount];
-        }
-        
-        // Base Case: If there is only one subarray left, then all of the remaining numbers
-        // must go in the current subarray. So return the sum of the remaining numbers.
-        if (subarrayCount == 1) {
-            return memo[currIndex][subarrayCount] = prefixSum[n] - prefixSum[currIndex];
-        }
-        
-        // Otherwise, use the recurrence relation to determine the minimum largest subarray
-        // sum between currIndex and the end of the array with subarrayCount subarrays remaining.
-        int minimumLargestSplitSum = INT_MAX;
-        for (int i = currIndex; i <= n - subarrayCount; i++) {
-            // Store the sum of the first subarray.
-            int firstSplitSum = prefixSum[i + 1] - prefixSum[currIndex];
-            
-            // Find the maximum subarray sum for the current first split.
-            int largestSplitSum = max(firstSplitSum, 
-                                      getMinimumLargestSplitSum(prefixSum, i + 1, subarrayCount - 1));
-            
-            // Find the minimum among all possible combinations.
-            minimumLargestSplitSum = min(minimumLargestSplitSum, largestSplitSum);
-             
-            if (firstSplitSum >= minimumLargestSplitSum) {
-                break;
-            }
-        }
-        
-        return memo[currIndex][subarrayCount] = minimumLargestSplitSum;
-    }
-                       
     int splitArray(vector<int>& nums, int m) {
-        // Marking all values to -1 so that we can differentiate 
-        // If we have already calculated the answer or not
-        memset(memo, -1, sizeof(memo));
-        
-        // Store the prefix sum of nums array.
-        int n = nums.size();
-        vector<int> prefixSum(n + 1, 0);
-        for (int i = 0; i < n; i++) {
-            prefixSum[i + 1] = prefixSum[i] + nums[i];
+        int n=nums.size();
+        vector<vector<int>> dp(n+1,vector<int> (m,-1));
+        return func(nums,  0, 0, m, n, dp);
+    }
+    
+    int func(vector<int>& nums, int i, int curr, int m, int n, vector<vector<int>> &dp){
+        if(curr==m && i==n){
+            return 0;
         }
         
-        return getMinimumLargestSplitSum(prefixSum, 0, m);
+        if(i==n){
+            return 100000000;
+        }
+        
+        if(curr>=m){
+            return 100000000;
+        }
+        
+        if(dp[i][curr]!=-1){
+            return dp[i][curr];
+        }
+        
+        int ans=max(nums[i],func(nums, i+1, curr+1, m, n, dp));
+        int sum=nums[i];
+        
+        for(int j=i+1;j<n;j++){
+            sum+=nums[j];
+            ans=min(ans,max(sum,func(nums, j+1, curr+1, m, n, dp)));
+        }
+        
+        return dp[i][curr]=ans;
     }
 };
