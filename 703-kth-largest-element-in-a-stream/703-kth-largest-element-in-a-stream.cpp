6c4003c49e1a4611bdce4dc497874b7c86a4bb44
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-04-08 19:18:07 +0530

    Time: 24 ms (99.34%), Space: 19.8 MB (63.09%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
@@ -1,18 +1,21 @@
 class KthLargest {
 public:
-    priority_queue<int, vector<int>, greater<int>> pq;
+    priority_queue<int, vector<int>, greater<int>> pq; /// to create a min heap
     int size;
     KthLargest(int k, vector<int> nums) {
         size=k;
         for(int i=0;i<nums.size();i++) {
             pq.push(nums[i]);
-            if(pq.size()>k) pq.pop();
+            if(pq.size()>k) pq.pop(); // dont confuge pop aage se ho raha hai
         }
+        
+        // auto it=pq.back();
+        // cout<<it<<endl;
     }
     
     int add(int val) {
         pq.push(val);
         if(pq.size()>size) pq.pop();
-        return pq.top();
+        return pq.top(); // sabse aage wala element
     }
 };
