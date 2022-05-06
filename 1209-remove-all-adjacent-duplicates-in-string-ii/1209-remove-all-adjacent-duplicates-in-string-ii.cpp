0f22b493227d41f74e94e97266ce5c0b22637565
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-05-06 18:56:10 +0530

    Time: 213 ms (10.53%), Space: 46.9 MB (6.97%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,18 +1,48 @@
-class Solution{
+class Solution {
 public:
-    string removeDuplicates(string s, int k){
-        vector<pair<char, short>> st;
-        string res;
-        for (auto ch : s){
-            if (st.empty() || st.back().first != ch)
-                st.push_back({ch, 0});
-            if (++st.back().second == k)
-                st.pop_back();
+    string removeDuplicates(string s, int k) {
+        unordered_map<int,pair<char,int>> mp;
+        vector<pair<char,int>> st;
+        int len=s.size();
+        
+        for(int i=0;i<len;i++){
+            // cout<<s[i]<<endl;
+            // for(auto it:st){
+            //     cout<<it.first;
+            // }
+            // cout<<endl;
+            if(st.empty()){
+                st.push_back(make_pair(s[i],i));
+                mp[i]=make_pair(s[i],1);
+            }
+            else{
+                auto mat=st.back();
+                // int ch=s[i];
+                if(mat.first==s[i]){
+                    if(mp[mat.second].second==k-1){
+                        int cnt=k-1;
+                        while(!st.empty() && st.back().first==s[i] && cnt){
+                            cnt--;
+                            st.pop_back();
+                        }
+                    }
+                    else{
+                        mp[i]=make_pair(s[i],mp[mat.second].second+1);
+                        st.push_back(make_pair(s[i],i));
+                    }
+                }
+                else{
+                    st.push_back(make_pair(s[i],i));
+                    mp[i]=make_pair(s[i],1);
+                }
+            }
         }
-        for (auto &p : st){
-            // cout<<string(p.second, p.first)<<endl; //////// to repeat a character n times
-            res += string(p.second, p.first);    
+        
+        string ans;
+        for(auto it:st){
+            ans.push_back(it.first);
         }
-        return res;
+        
+        return ans;
     }
 };
