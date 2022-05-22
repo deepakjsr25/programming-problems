184825443dbc19204a2906bd8a9bf9b75c833535
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-05-22 13:30:27 +0530

    Time: 35 ms (29.77%), Space: 20.2 MB (18.18%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,45 +1,31 @@
 class Solution {
 public:
     int countSubstrings(string s) {
-        int len = s.length();
-        int cnt =0;
-        int left,right;
-        for(int i=0;i<len;i++)
-        {
-            cnt++;
-            left = i-1;
-            right = i+1;
-            while(left>=0 && right<len)
-            {
-                if(s[left] == s[right])
-                    cnt++;
-                else
-                    break;
-                left--;
-                right++;
+        int len=s.length();
+        vector<vector<int>> dp(len+1,vector<int> (len+1,0));
+        int count=0;
+        
+        for(int i=0;i<len;i++){
+            dp[i][i]=1;
+            count++;
+            if(s[i]==s[i+1]){
+                dp[i][i+1]=1;
+                count++;
             }
         }
         
-        for(int i=0;i<len-1;i++)
-        {
-            if(s[i]!=s[i+1])
-                continue;
-            
-            cnt++;
-            left = i-1;
-            right = i+2;
-            
-            while(left>=0 && right<len)
-            {
-                if(s[left] == s[right])
-                    cnt++;
-                else
-                    break;
-                left--;
-                right++;
+        for(int i=len-3;i>=0;i--){
+            for(int j=len-1;j>=0 && j>i+1;j--){
+                if(s[i]==s[j]){
+                    dp[i][j]=dp[i+1][j-1];
+                }
+                
+                if(dp[i][j]){
+                    count++;
+                }
             }
         }
         
-        return cnt;
+        return count;
     }
 };
