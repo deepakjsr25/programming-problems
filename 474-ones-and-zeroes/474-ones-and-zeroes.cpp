3b52e695140f286b921f14c18a47400d04eb1c7f
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-05-24 01:40:03 +0530

    Time: 205 ms (89.03%), Space: 32.3 MB (59.66%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,31 +1,47 @@
 class Solution {
 public:
-    int findMaxForm(vector<string>& strs, int m, int n) {
-  vector<vector<int>> memo(m+1, vector<int>(n+1, 0));
-  int numZeroes, numOnes;
-
-  for (auto &s : strs) {
-    numZeroes = numOnes = 0;
-    // count number of zeroes and ones in current string
-    for (auto c : s) {
-      if (c == '0')
-	numZeroes++;
-      else if (c == '1')
-	numOnes++;
+    int findMaxForm(vector<string>& strs, int m, int n)
+    {
+        int sz = strs.size();
+ 
+        int dp[sz+1][m+1][n+1];
+ 
+        for(int i=0;i<=sz;i++)
+        {
+            for(int j=0;j<=m;j++)
+            {
+                for(int k=0;k<=n;k++)
+                    dp[i][j][k] = 0;
+            }
+        }
+ 
+        int ans = 0;
+ 
+        // fill up the 3D dp table
+ 
+        for(int i=1;i<=sz;i++)
+        {
+            int ones = count(strs[i-1].begin(),strs[i-1].end(),'1');
+            int zeroes = strs[i-1].length() - ones;
+ 
+            for(int j=0;j<=m;j++)
+            {
+                for(int k=0;k<=n;k++)
+                {
+                    // current string not choosen
+                    dp[i][j][k] = dp[i-1][j][k];
+ 
+                    // check if current string can be choosen or not
+                    if(j-zeroes>=0 and k-ones>=0)
+                        dp[i][j][k] = max(dp[i][j][k],1+dp[i-1][j-zeroes][k-ones]);
+ 
+                    ans = max(ans,dp[i][j][k]);
+ 
+                }
+            }
+        }
+ 
+        return ans;
+ 
     }
-
-    // memo[i][j] = the max number of strings that can be formed with i 0's and j 1's
-    // from the first few strings up to the current string s
-    // Catch: have to go from bottom right to top left
-    // Why? If a cell in the memo is updated(because s is selected),
-    // we should be adding 1 to memo[i][j] from the previous iteration (when we were not considering s)
-    // If we go from top left to bottom right, we would be using results from this iteration => overcounting
-    for (int i = m; i >= numZeroes; i--) {
-	for (int j = n; j >= numOnes; j--) {
-          memo[i][j] = max(memo[i][j], memo[i - numZeroes][j - numOnes] + 1);
-	}
-    }
-  }
-  return memo[m][n];
-}
 };
