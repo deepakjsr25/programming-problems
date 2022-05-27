03166cb307372240676032feca6d51a7c0724462
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-05-28 01:23:11 +0530

    Time: 24 ms (32.87%), Space: 138.4 MB (18.18%) - LeetHub

--- 808-soup-servings/808-soup-servings.cpp
+++ 808-soup-servings/808-soup-servings.cpp
@@ -1,25 +1,38 @@
 class Solution {
 public:
     double soupServings(int n) {
-        if(n == 0)
+        if(n==0){
             return 0.5;
-        if(n > 4800)
-            return 1.0;
-        vector<vector<double>> memo(n + 1, vector<double>(n + 1, -1));
-        dp(memo, n, n);
-        return memo[n][n];
-    }
-    double dp(vector<vector<double>> &memo, int A, int B){
-        if(!A && !B)
-            return 0.5;
-        if(!A || !B)
-            return !A ? 1.0 : 0;
-        if(memo[A][B] != -1)
-            return memo[A][B];
-        double ans = 0;
-        for(int i = 25; i <= 100; i += 25){
-            ans += 0.25 * dp(memo, max(0, A - i), max(0, B - 100 + i));
         }
-        return memo[A][B] = ans;
+        
+        if(n>4800){
+            return 1.0;
+        }
+        
+        vector<vector<double>> dp(n+1,vector<double> (n+1,-1));
+        soup(dp,n,n);
+        
+        return dp[n][n];
+    }
+    
+    double soup(vector<vector<double>>& dp,int a,int b){
+        if(!a && !b){
+            return 0.5;
+        }
+        
+        if(!a || !b){
+            return (!a)?1:0;
+        }
+        
+        if(dp[a][b]!=-1){
+            return dp[a][b];
+        }
+        
+        double cnt=0.0;
+        for(int i=25;i<=100;i+=25){
+            cnt+=0.25*soup(dp,max(0,a-i),max(0,b-100+i));
+        }
+        
+        return dp[a][b]=cnt; 
     }
 };
