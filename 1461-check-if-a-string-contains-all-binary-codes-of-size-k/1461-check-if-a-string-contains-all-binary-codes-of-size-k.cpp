88c465bd09437ffd8e3aae96eac0bef7ad272239
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-05-31 23:30:48 +0530

    Time: 995 ms (21.69%), Space: 98.6 MB (6.60%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -6,16 +6,13 @@ public:
         unordered_map<string,int> m;
         for(int i=0;i<len-k+1;i++){
             string st=s.substr(i,k);
-            // cout<<st<<endl;
             if(!m[st]){
                 cnt++;
             }
-            
-            m[st]++;
+            m[st]=1;
         }
         
         int req=1<<k;
-        // cout<<req<<" "<<cnt<<endl;
         if(cnt==req){
             return 1;
         }
