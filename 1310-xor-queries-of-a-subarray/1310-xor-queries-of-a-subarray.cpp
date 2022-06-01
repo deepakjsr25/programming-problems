0441055c1bf94ac1b1dc913bbe5c38f3701618e6
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-06-01 21:13:06 +0530

    Time: 227 ms (5.02%), Space: 41.2 MB (12.44%) - LeetHub

---
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+        int n=arr.size();
+        vector<int> prefix_sum(n);
+        vector<int> ans;
+        
+        prefix_sum[0]=arr[0];
+        for(int i=1;i<n;i++){
+            prefix_sum[i]=prefix_sum[i-1]^arr[i];
+            cout<<prefix_sum[i]<<" "<<arr[i]<<endl;
+        }
+        
+        for(auto it: queries){
+            if(it[0]==0){
+                ans.push_back(prefix_sum[it[1]]);
+            }
+            else{
+                ans.push_back(prefix_sum[it[0]-1]^prefix_sum[it[1]]);
+            }
+        }
+        
+        return ans;
+    }
+};
