ee298f38030115418bc2ffa7747b144719609d03
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-06-05 23:00:45 +0530

    Time: 7 ms (58.17%), Space: 6.2 MB (46.51%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -1,57 +1,8 @@
-// class Solution {
-// public:
-//     int totalNQueens(int n) {
-//         string s(n,'.');
-//         vector<string> v(n,s);
-//         int cnt=0;
-//         total(v,0,n,cnt);
-        
-//         return cnt;
-//     }
-    
-//     void total(vector<string> &v,int row,int n,int &cnt){
-//         if(row>=n){
-//             cnt++;
-//             return;
-//         }
-        
-//         for(int col=0;col<n;col++){
-//             if(check(v,row,col,n)){
-//                 v[row][col]='Q';
-//                 total(v,row+1,n,cnt);
-//                 v[row][col]='.';
-//             }
-//         }
-        
-//         return;
-//     }
-    
-//     bool check(vector<string> &v,int row,int col,int n){
-//         for(int k=0;k<n;k++){
-//             if(v[k][col]=='Q'){
-//                 return 0;
-//             }
-            
-//             if(row-k>=0 && col-k>=0 && v[row-k][col-k]=='Q'){
-//                 return 0;
-//             }
-            
-//             if(row-k>=0 && col+k>=0 && v[row-k][col+k]=='Q'){
-//                 return 0;
-//             }
-//         }
-        
-//         return 1;
-//     }
-// };
-
 class Solution {
 public:
     int totalNQueens(int n) {
         string s(n,'.');
         vector<string> v(n,s);
-        // int cnt=0;
-        
         
         return total(v,0,n);
     }
