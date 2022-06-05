ae4a673f1b73408a67a365b266b61b03e91f66d3
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-06-05 23:00:16 +0530

    Time: 11 ms (39.00%), Space: 6 MB (79.40%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -1,29 +1,76 @@
+// class Solution {
+// public:
+//     int totalNQueens(int n) {
+//         string s(n,'.');
+//         vector<string> v(n,s);
+//         int cnt=0;
+//         total(v,0,n,cnt);
+        
+//         return cnt;
+//     }
+    
+//     void total(vector<string> &v,int row,int n,int &cnt){
+//         if(row>=n){
+//             cnt++;
+//             return;
+//         }
+        
+//         for(int col=0;col<n;col++){
+//             if(check(v,row,col,n)){
+//                 v[row][col]='Q';
+//                 total(v,row+1,n,cnt);
+//                 v[row][col]='.';
+//             }
+//         }
+        
+//         return;
+//     }
+    
+//     bool check(vector<string> &v,int row,int col,int n){
+//         for(int k=0;k<n;k++){
+//             if(v[k][col]=='Q'){
+//                 return 0;
+//             }
+            
+//             if(row-k>=0 && col-k>=0 && v[row-k][col-k]=='Q'){
+//                 return 0;
+//             }
+            
+//             if(row-k>=0 && col+k>=0 && v[row-k][col+k]=='Q'){
+//                 return 0;
+//             }
+//         }
+        
+//         return 1;
+//     }
+// };
+
 class Solution {
 public:
     int totalNQueens(int n) {
         string s(n,'.');
         vector<string> v(n,s);
-        int cnt=0;
-        total(v,0,n,cnt);
+        // int cnt=0;
         
-        return cnt;
+        
+        return total(v,0,n);
     }
     
-    void total(vector<string> &v,int row,int n,int &cnt){
+    int total(vector<string> &v,int row,int n){
         if(row>=n){
-            cnt++;
-            return;
+            return 1;
         }
         
+        int cnt=0;
         for(int col=0;col<n;col++){
             if(check(v,row,col,n)){
                 v[row][col]='Q';
-                total(v,row+1,n,cnt);
+                cnt+=total(v,row+1,n);
                 v[row][col]='.';
             }
         }
         
-        return;
+        return cnt;
     }
     
     bool check(vector<string> &v,int row,int col,int n){
