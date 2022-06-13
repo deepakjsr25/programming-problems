1c3d698d63a13bbbe6301893fc4501c00a0d92e9
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-06-13 19:57:38 +0530

    Time: 7 ms (75.60%), Space: 8.6 MB (70.75%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -2,20 +2,26 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& t) {
         int sum=0;
-        int row=t.size(),col=t[row-1].size();
-        vector<vector<int>> dp(row,vector<int> (col,20000000));
-        return minimum(t,0,0,row,col,dp);
+        int n=t.size();
+        
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<t[i].size();j++){
+                t[i][j]+=min(t[i+1][j],t[i+1][j+1]);
+            }
+        }
+        
+        return t[0][0];
     }
     
-    int minimum(vector<vector<int>>& t,int i,int j,int row,int col,vector<vector<int>>& dp){
-        if(i>=row){
-            return 0; 
-        }
+//     int minimum(vector<vector<int>>& t,int i,int j,int row,int col,vector<vector<int>>& dp){
+//         if(i>=row){
+//             return 0; 
+//         }
         
-        if(dp[i][j]!=20000000){
-            return dp[i][j];
-        }
+//         if(dp[i][j]!=20000000){
+//             return dp[i][j];
+//         }
         
-        return dp[i][j]=t[i][j]+min(minimum(t,i+1,j,row,col,dp),minimum(t,i+1,j+1,row,col,dp));
-    }
+//         return dp[i][j]=t[i][j]+min(minimum(t,i+1,j,row,col,dp),minimum(t,i+1,j+1,row,col,dp));
+//     }
 };
