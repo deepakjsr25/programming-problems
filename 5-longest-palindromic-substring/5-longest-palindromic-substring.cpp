24e7972b05d498424e8d8650580dea6c0799235c
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-06-16 22:37:37 +0530

    Time: 383 ms (32.53%), Space: 11.5 MB (55.34%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,45 +1,33 @@
 class Solution {
-public:
-    string longestPalindrome(string s) {
-        int n=s.length();
-        vector<vector<int>> dp(n,vector<int> (n,0));
-        
-        for(int i=0;i<n;i++){
-            dp[i][i]=1;
-        }
-        
-        string st="";
-        int maxi=0;
-        for(int i=0;i<n-1;i++){
-            // cout<<s[i+1]<<endl;
-            if(s[i]==s[i+1]){
-                // cout<<i<<" "<<endl;
-                dp[i][i+1]=2;
-                st=s.substr(i,2);
-                maxi=2;
-            }
-        }
-        
-        for(int i=n-3;i>=0;i--){
-            for(int j=n-1;j>i+1;j--){
-                // cout<<i<<" "<<endl;
-                if(s[i]==s[j] && dp[i+1][j-1]>0){
-                    dp[i][j]=dp[i+1][j-1]+2;
-                }
-                
-                if(dp[i][j]>maxi){
-                    maxi=dp[i][j];
-                    st=s.substr(i,dp[i][j]);
-                }
-                // cout<<"erroe"<<endl;
-            }
-        }
-        
-        if(maxi==0){
-            st=s[0];
-        }
-        
-        // cout<<st<<endl;
-        return st;
-    }
+	public:
+   	string longestPalindrome(string s) {   
+	    int n = s.size();
+    	int dp[n][n];
+    	
+	    memset(dp,0,sizeof(dp));
+    	int end=1;
+    	int strt=0;
+	
+	    for(int i=0;i<n;i++){
+	        dp[i][i] = 1;
+    	}
+	    for(int i=0;i<n-1;i++){
+	        if(s[i]==s[i+1]){
+    	    	dp[i][i+1]=1;
+        		strt=i;end=2;
+       		}
+	    }
+    	for(int j=2;j<n;j++){
+	        for(int i=0;i< n-j;i++){  
+           		int lft=i;
+            	int rght = i+j;
+        	    
+    	        if(dp[lft+1][rght-1]==1 && s[lft]==s[rght]) 
+	            {
+                	dp[lft][rght]=1; strt=i; end=j+1; 
+            	}        
+        	}
+    	}
+    return s.substr(strt, end);
+	}
 };
