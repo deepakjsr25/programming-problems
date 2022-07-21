603c9fa68505bf082fd411ccfc03ebc2ed859e27
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-07-21 16:16:33 +0530

    Time: 3 ms (72.05%), Space: 7.5 MB (17.76%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
@@ -8,79 +8,21 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
-public:
-    ListNode* reverseBetween(ListNode* head, int left, int right) {
-        if(head==NULL){
-            return head;
+class Solution{
+    public:
+        ListNode* reverseBetween(ListNode *head, int left, int right){
+            ListNode *dummy = new ListNode(0), *pre = dummy, *cur;
+            dummy->next = head;
+            for (int i = 0; i < left - 1; i++){
+                pre = pre->next;
+            }
+            cur = pre->next;
+            for (int i = 0; i < right - left; i++){
+                ListNode *temp = pre->next;
+                pre->next = cur->next;
+                cur->next = cur->next->next;
+                pre->next->next = temp;
+            }
+            return dummy->next;
         }
-        
-        if(left==right){
-            return head;
-        }
-        
-        ListNode* temp=head;
-        ListNode* prev=NULL;
-        int cnt1=left-1,cnt2=right-1; //0 , 1
-        
-        while(cnt1!=0){
-            prev=temp;
-            temp=temp->next;
-            cnt1--;
-            cnt2--;
-        }
-        
-        while(cnt2!=0){
-            temp=temp->next;
-            cnt2--;
-        }
-        
-        ListNode* after=temp->next;
-        
-        if(prev!=NULL){
-            temp=prev->next;
-        }
-        else{
-            temp=head;
-        }
-        
-        ListNode* rev=after; //null
-        cnt1=right-left;
-        while(cnt1!=0){
-            ListNode* newnode=new ListNode(temp->val);
-            newnode->next=rev;
-            rev=newnode;
-            temp=temp->next;
-            cnt1--;
-        }
-        
-        ListNode* newnode=new ListNode(temp->val);
-        newnode->next=rev;
-        rev=newnode;
-        
-        if(prev!=NULL){
-            prev->next=rev;
-        }
-        else{
-            head=rev;
-        }
-        
-        return head;
-    }
-    
-//     ListNode* reverse(ListNode* temp, int right){
-//         if(head==NULL){
-//             return head;
-//         }
-        
-//         if(temp->val==right){
-//             return temp;
-//         }
-        
-//         ListNode* newnode=new ListNode(left);
-//         ListNode* rev=reverse(temp->next,right);
-//         ListNode* temp=rev->next;
-//         rev->next=newnode;
-//         newnode->next=temp;
-//     }
 };
