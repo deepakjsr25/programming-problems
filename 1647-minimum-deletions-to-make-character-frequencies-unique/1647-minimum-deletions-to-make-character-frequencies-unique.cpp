291e31210e4e0dee76091f862f931d870cda1b7f
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-07-25 01:20:32 +0530

    Time: 95 ms (49.07%), Space: 17.8 MB (7.80%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,32 +1,53 @@
 class Solution {
 public:
     int minDeletions(string s) {
-        unordered_map<int,int>charcnt;
-        unordered_map<int,int>uniquefreq;
-        int count=0;
+        int n=s.length();
+        int v[26]={0};
+        int maxi=0;
         
-        for(int i=0;i<s.size();i++)
-        {
-            charcnt[s[i]-'a']++;
+        for(int i=0;i<n;i++){
+            v[s[i]-'a']++;
+            maxi=max(maxi,v[s[i]-'a']);
+        }
+    
+        vector<int> v2(maxi+1,0);
+        for(int i=0;i<26;i++){
+            if(v[i]>0){
+                v2[v[i]]++;
+                // cout<<v[i]<<endl;
+            }
         }
         
-        for(auto &it : charcnt)
-        {
-            int x= it.second;
-            if(uniquefreq[x]!=0)
-            {
-                while(x>0 && uniquefreq[x]!=0)
-                {
-                    x--;
-                    count++;
+        // cout<<maxi<<endl;
+        // for(int i=0;i<=maxi;i++){
+        //     cout<<i<<" "<<v2[i]<<endl;
+        // }
+        
+        int i=maxi,j=maxi,ans=0;
+        while(j>=0 && i>=1){
+            while(v2[i]>1 && j>=0){
+                if(j==0 || v2[j]==0){
+                    ans+=(i-j);
+                    // cout<<i<<" "<<j<<" "<<i-j<<endl;
+                    v2[j]=1;
+                    v2[i]--;
+                }
+                if(j!=0){
+                    j--;
                 }
             }
-            if(x>0)
-            {
-                uniquefreq[x]++;
+            if(i==j){
+                j--;
             }
+            
+            i--;
         }
         
-        return count;
+        return ans;
     }
 };
+
+
+// "adiiijjjjaabbbbjdkkedddeffhhhhiiekkeeffifgghhjfggggjjhikkkk
+
+// "accdcdadddbaadbc"
