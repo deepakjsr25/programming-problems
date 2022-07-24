a059972c16c98411f4b456b3d98d05272efdb536
Deepak Kumar <68379114+deepakjsr25@users.noreply.github.com>
2022-07-24 23:29:12 +0530

    Time: 555 ms (7.63%), Space: 14.8 MB (68.84%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -3,13 +3,10 @@ public:
     bool searchMatrix(vector<vector<int>>& mat, int target) {
         int m=mat.size();
         
-        int i=0;
-        while(i<m){
+        for(int i=0;i<m;i++){
             if(binary_search(mat[i].begin(),mat[i].end(),target)){
                 return 1;
             }
-            
-            i++;
         }
         
         return 0;
